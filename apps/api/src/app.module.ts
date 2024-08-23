c14e566ed12b7f6a34fef48e85e1b819e12ac703
Shray Jain <76147471+shray-jayn@users.noreply.github.com>
2024-08-23 17:19:49 +0530

    chore: add nestjs-prisma-codegen and setup graphQl and Rest models and service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,11 +5,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { PrismaModule } from './common/prisma/prisma.module';
+import { UsersModule } from './models/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
+const MAX_AGE = 24 * 60 * 60 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    JwtModule.register({
+      global:true,
+      secret:process.env.JWT_SECRET,
+      signOptions: {expiresIn: MAX_AGE}
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       introspection: true,
@@ -19,7 +27,9 @@ import { UsersModule } from './users/users.module';
         numberScalarMode: 'integer',
       },
     }),
-    UsersModule,
+
+    PrismaModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
