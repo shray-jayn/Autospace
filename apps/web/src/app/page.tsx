23712cb7fce646585dca673cce9f47075189d0e1
Shray Jain <76147471+shray-jayn@users.noreply.github.com>
2024-09-06 23:06:01 +0530

    chore: add UI library

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,16 +2,20 @@
 import { add } from '@autospace/sample-lib'
 import { useQuery } from '@apollo/client'
 import { CompaniesDocument } from '@autospace/network/src/gql/generated'
+import { BrandIcon } from '@autospace/ui/src/components/atoms/BrandIcon'
+import { Button} from '@autospace/ui/src/components/atoms/Button'
 
 export default function Home() {
   const { data, loading } = useQuery(CompaniesDocument)
 
   return (
-    <main>
+    <main className='bg-primary'>
+      <BrandIcon/>
+      <Button/>
       Hello {add({ a: 342, b: 12 })}
       <div>
         {data?.companies.map((company) => (
-          <div className="p-4 bg-gray-100 rounded" key={company.id}>
+          <div className="p-4 rounded" key={company.id}>
             <div>{company.description}</div>
             <div>{company.description}</div>
           </div>
